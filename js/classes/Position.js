840975edb2b47f5b0d8a565136cc79fc732996c4
Li-Ta Lo <lita.ollie@gmail.com>
2015-02-22 16:47:34 -0700

    Position
    Add implementation of the constructor of Position.

---
+++ js/classes/Position.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ollie on 2/22/15.
+ */
+
+function Position(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+module.exports = Position;
--- js/test/testPosition.js
+++ js/test/testPosition.js
@@ -2,12 +2,14 @@
  * Created by ollie on 2/22/15.
  */
 
+Position = require('../classes/Position');
+
 describe('Position', function () {
    describe('Constructor', function () {
        it('should initialize the x and y component', function () {
            var position = new Position(1, 2);
            position.x.should.be.exactly(1);
            position.y.should.be.exactly(2);
-       })
-   }) 
+       });
+   });
 });
