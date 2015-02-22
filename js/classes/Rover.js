eb728d2e62e0138c8a05d6a48a1526d0629fdcf8
Li-Ta Lo <lita.ollie@gmail.com>
2015-02-22 09:47:55 -0700

    Trivial Rover Constructor
    Add a trivial implementation of the Rover constructor. The Rover constructor is in its own module and exported to the test module.

---
+++ js/classes/Rover.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ollie on 2/22/15.
+ */
+
+function Rover(x, y, orientation) {
+    
+};
+
+module.exports = Rover;
--- js/test/testRover.js
+++ js/test/testRover.js
@@ -2,10 +2,12 @@
  * Created by ollie on 2/22/15.
  */
 
+var Rover = require('../classes/Rover');
+
 describe('A Mars Rover', function () {
     describe('Constructor', function () {
         it('should initialize the Rover with the given position and orientation', function () {
             var rover = new Rover(0, 0, 'N');
         })
     })
-})
+});
