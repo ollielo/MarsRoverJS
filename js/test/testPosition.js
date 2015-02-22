ba86e834f3b96c8dd9e7561cca735629326191a0
Li-Ta Lo <lita.ollie@gmail.com>
2015-02-22 16:46:27 -0700

    Position
    Add failing test on the contructor of Position.

---
+++ js/test/testPosition.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ollie on 2/22/15.
+ */
+
+describe('Position', function () {
+   describe('Constructor', function () {
+       it('should initialize the x and y component', function () {
+           var position = new Position(1, 2);
+           position.x.should.be.exactly(1);
+           position.y.should.be.exactly(2);
+       })
+   }) 
+});
