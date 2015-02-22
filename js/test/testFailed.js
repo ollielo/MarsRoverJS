d45b03b1e668d8de963df22357cc53748909faf2
Li-Ta Lo <lita.ollie@gmail.com>
2015-02-22 09:28:27 -0700

    First passing test
    Made the failing test pass.

--- js/test/testFailed.js
+++ js/test/testFailed.js
@@ -6,6 +6,6 @@ var assert = require("should");
 
 describe("A failing test", function () {
     it("should fail", function () {
-        assert.equal(1, 0);
+        assert.equal(1, 1);
     })
 });
