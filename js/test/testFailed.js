02636370141a242391714c9e51e8a9ef5ee23208
Li-Ta Lo <lita.ollie@gmail.com>
2015-02-22 09:27:47 -0700

    First failing test
    Add first failing test as a check for the Mocha test framework.

---
+++ js/test/testFailed.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ollie on 2/22/15.
+ */
+
+var assert = require("should");
+
+describe("A failing test", function () {
+    it("should fail", function () {
+        assert.equal(1, 0);
+    })
+});
