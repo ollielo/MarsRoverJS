457fc2ff6742b0d58443c0c9287b3d3218d37c0d
Li-Ta Lo <lita.ollie@gmail.com>
2015-02-22 13:07:35 -0700

    Trivially Receiving Commands
    Add trivial implementation for receiving commands

--- js/classes/Rover.js
+++ js/classes/Rover.js
@@ -6,6 +6,10 @@ function Rover(x, y, orientation) {
     this.x = x;
     this.y = y;
     this.orientation = orientation;
+    
+    this.receiveCommands = function (cmds) {
+        
+    }
 };
 
 module.exports = Rover;
