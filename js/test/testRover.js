2eff1b7eff9297c1463d479ba188bac1fe6b83f2
Li-Ta Lo <lita.ollie@gmail.com>
2015-02-22 09:36:47 -0700

    Failing Rover Constructor
    Add a failing test on the constructor

---
+++ js/test/testRover.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ollie on 2/22/15.
+ */
+
+describe('A Mars Rover', function () {
+    describe('Constructor', function () {
+        it('should initialize the Rover with the given position and orientation', function () {
+            var rover = new Rover(0, 0, 'N');
+        })
+    })
+})
