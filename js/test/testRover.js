4a9e535d0245e7bd890c42e3574a9cc7d5e13603
Li-Ta Lo <lita.ollie@gmail.com>
2015-02-22 09:53:02 -0700

    Failing tests on Rover object
    Add failing test on the content of the Rover object.

--- js/test/testRover.js
+++ js/test/testRover.js
@@ -8,6 +8,9 @@ describe('A Mars Rover', function () {
     describe('Constructor', function () {
         it('should initialize the Rover with the given position and orientation', function () {
             var rover = new Rover(0, 0, 'N');
+            rover.x.should.be.exactly(0);
+            rover.y.should.be.exactly(0);
+            rover.orientation.should.be.exactly('N');
         })
     })
 });
