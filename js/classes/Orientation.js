5580d087438bb6f76dbffd16336c528d1feff227
Li-Ta Lo <lita.ollie@gmail.com>
2015-02-22 19:24:50 -0700

    Refactor North
    Refactor North.advance as a inline method.

--- js/classes/Orientation.js
+++ js/classes/Orientation.js
@@ -2,12 +2,14 @@
  * Created by ollie on 2/22/15.
  */
 
-var North = {name: "North"};
-North.advance = function(dir) {
-    return {
-        'f': {x: 0, y: 1},
-        'b': {x: 0, y: -1}
-    }[dir];
+var North = {
+    name: "North",
+    advance: function (dir) {
+        return {
+            'f': {x: 0, y: 1},
+            'b': {x: 0, y: -1}
+        }[dir];
+    }
 };
 
 var South = {
